cb9458e21a21df611c9ab7a7d16327155ac7f4a3
J35P312 <jesper.eisfeldt@scilifelab.se>
2016-03-04 15:25:37 +0100

    modified:   FindSV.py 	modified:   FindSV_modules.py 	modified:   config.txt 	modified:   generate_Json_config.py

--- FindSV.py
+++ FindSV.py
@@ -21,21 +21,48 @@ def main(args):
     config=readconfig(args.config,args);
     caller_slurm_ID=[];
     caller_output=[];
-    
+    #fetch the scripts
     scripts=FindSV_modules.main()
+    #initiate the output location
+    if(args.output):
+        output=args.output
+    else:
+        output=config["FindSV"]["general"]["output"]
+    #the prefix of the output is set to the prefix of the bam file
+    prefix=args.bam.split("/")[-1]
+    prefix=prefix.replace(".bam","")
+        
     #run the callers
+    input_vcf=""
     for caller in config["FindSV"]["calling"]:
-        ID=[];OUT=[];
-        print(caller)
-        print(scripts["FindSV"]["calling"][caller])
+        general_config=config["FindSV"]["general"]
+        caller_config=config["FindSV"]["calling"][caller]
+        if caller == "FT":
+            FT=scripts["FindSV"]["calling"][caller].format(account=general_config["account"],output=output,prefix=prefix,time="30:00:00",FT_path=caller_config["FT_path"],bam_path=args.bam,minimum_suporting=caller_config["minimum_supporting_pairs"])
+            print(FT)
+            input_vcf += "{}/{}_FT_inter_chromosomal.vcf ".format(output,prefix)
+            input_vcf += "{}/{}_FT_intra_chromosomal.vcf ".format(output,prefix)
+        elif caller =="CNVnator":
+            CNVNator=scripts["FindSV"]["calling"][caller].format(account=general_config["account"],output=output,prefix=prefix,time="30:00:00",CNVnator_path=caller_config["CNVnator_path"],bam_path=args.bam,bin_size=caller_config["bin_size"],reference_dir=caller_config["reference_dir"])
+            print(CNVNator)
+            input_vcf += "{}/{}_CNVnator.vcf ".format(output,prefix)
     #combine module; combine all the caller modules into one VCF
-    print(scripts["FindSV"]["combine"]["combine"])
+    combine_config=config["FindSV"]["combine"]
+    combine=scripts["FindSV"]["combine"]["combine"].format(account=general_config["account"],output=output,prefix=prefix,time="3:00:00",merge_vcf_path=combine_config["merge_vcf_path"],input_vcf=input_vcf,contig_sort_path=combine_config["contig_sort_path"],bam_path=args.bam)
+    print(combine)
+    
     #annotation module; filter and annotate the samples
-    print(scripts["FindSV"]["annotation"]["header"])
-    print(scripts["FindSV"]["annotation"]["DB"])
-    print(scripts["FindSV"]["annotation"]["VEP"])
-    print(scripts["FindSV"]["annotation"]["GENMOD"])
-    print(scripts["FindSV"]["annotation"]["cleaning"])
+    annotation_config=config["FindSV"]["annotation"]
+    
+    annotation=scripts["FindSV"]["annotation"]["header"].format(account=general_config["account"],output=output,prefix=prefix,time="10:00:00")
+    annotation += scripts["FindSV"]["annotation"]["DB"].format(query_script=annotation_config["DB"]["DB_script_path"],output=output,prefix=prefix,db_folder_path=annotation_config["DB"]["DB_path"])
+    cache_dir=""
+    if not annotation_config["VEP"]["cache_dir"] == "":
+        cache_dir=" --dir {}".format(annotation_config["VEP"]["cache_dir"])
+    annotation += scripts["FindSV"]["annotation"]["VEP"].format(vep_path=annotation_config["VEP"]["VEP.pl_path"],output=output,prefix=prefix,port=annotation_config["VEP"]["port"],cache_dir=cache_dir)
+    annotation += scripts["FindSV"]["annotation"]["GENMOD"].format(genmod_score_path=annotation_config["GENMOD"]["GENMOD_rank_model_path"],output=output,prefix=prefix)
+    annotation += scripts["FindSV"]["annotation"]["cleaning"].format(output=output,prefix=prefix,VCFTOOLS_path=annotation_config["VCFTOOLS"]["VCFTOOLS_path"])
+    print(annotation)
     return(None)
 
 
--- FindSV_modules.py
+++ FindSV_modules.py
@@ -43,8 +43,8 @@ rm {output}/{prefix}.cnvnator.out
 #SBATCH -p core
 #SBATCH -t {time}
 
-sbatch.write("python {merge_vcf_path} --vcf {input_vcf} > {output}/{prefix}_FindSV.unsorted.vcf
-sbatch.write( "python {contig_sort_path} --vcf {output}/{prefix}_FindSV.unsorted.vcf --bam {bam_path} > {output}/{prefix}_FindSV.vcf
+python {merge_vcf_path} --vcf {input_vcf} > {output}/{prefix}_FindSV.unsorted.vcf
+python {contig_sort_path} --vcf {output}/{prefix}_FindSV.unsorted.vcf --bam {bam_path} > {output}/{prefix}_FindSV.vcf
 rm {output}/{prefix}_FindSV.unsorted.vcf"""
     combine={"combine":combine}
     
@@ -58,13 +58,13 @@ rm {output}/{prefix}_FindSV.unsorted.vcf"""
 
 """
 #the DB section
-    DB="python {query_script} --variations {output}/{prefix}_FindSV.vcf} --db {db_folder_path} > {output}/{prefix}_frq.vcf\n"
+    DB="python {query_script} --variations {output}/{prefix}_FindSV.vcf --db {db_folder_path} > {output}/{prefix}_frq.vcf\n"
 #the vep section
     VEP="perl {vep_path} --cache --force_overwrite --poly b -i {output}/{prefix}_frq.vcf -o {output}/{prefix}_vep.vcf --buffer_size 5 --port {port} --vcf --per_gene --format vcf  {cache_dir} -q\n"
 #the genmod section
-    GENMOD="genmod score -c {genmod_score_path} {output}/{prefix}_vep.vcf} > {output}/{prefix}_vep.vcf}.tmp\nmv output}/{prefix}_vep.vcf}.tmp {output}/{prefix}_vep.vcf\n"
+    GENMOD="genmod score -c {genmod_score_path} {output}/{prefix}_vep.vcf > {output}/{prefix}_vep.vcf.tmp\nmv {output}/{prefix}_vep.vcf.tmp {output}/{prefix}_vep.vcf\n"
 #the cleaning sections
-    cleaning="vcftools --recode --recode-INFO-all --remove-filtered-all --vcf {output}/{prefix}_vep.vcf --stdout > {output}/{prefix}_FindSV_clean.vcf \n"
+    cleaning="{VCFTOOLS_path} --recode --recode-INFO-all --remove-filtered-all --vcf {output}/{prefix}_vep.vcf --stdout > {output}/{prefix}_FindSV_clean.vcf \n"
     filter={"header":header,"VEP":VEP,"DB":DB,"GENMOD":GENMOD,"cleaning":cleaning}
     
     scripts={"FindSV":{"calling":calling,"annotation":filter,"combine":combine}}
