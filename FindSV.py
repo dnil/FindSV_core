7c129e1d30ac66717bf0c31c215dc193bea0982e
J35P312 <jesper.eisfeldt@scilifelab.se>
2016-03-06 15:51:13 +0100

    modified:   FindSV.py 	modified:   FindSV_modules.py 	modified:   config.txt 	new file:   internal_scripts/cleanVCF.py 	new file:   internal_scripts/contigSort.py 	renamed:    generate_Json_config.py -> internal_scripts/generate_Json_config.py 	new file:   internal_scripts/mergeVCF.py

--- FindSV.py
+++ FindSV.py
@@ -3,6 +3,21 @@ import sys
 import yaml
 import os
 import FindSV_modules
+import subprocess
+
+#this function prints the scripts, submits the slurm job, and then returns the jobid
+def submitSlurmJob(path,message):
+    slurm=open( path ,"w")
+    slurm.write(message)
+    slurm.close()
+
+    process = "sbatch {0}".format(path)
+    p_handle = subprocess.Popen(process, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)    
+    p_out, p_err = p_handle.communicate()
+    try:
+        return( re.match(r'Submitted batch job (\d+)', p_out).groups()[0] );
+    except:
+        return("123456")
 
 #read the config file, prefer command line option to config
 def readconfig(path,command_line):
@@ -18,6 +33,7 @@ def readconfig(path,command_line):
     return(config)
 
 def main(args):
+    programDirectory = os.path.dirname(os.path.abspath(__file__))
     config=readconfig(args.config,args);
     caller_slurm_ID=[];
     caller_output=[];
@@ -28,42 +44,71 @@ def main(args):
         output=args.output
     else:
         output=config["FindSV"]["general"]["output"]
+    #create a folder to keep the output sbatch scripts and logs
+    if not os.path.exists(os.path.join(output,"slurm/calling/")):
+        os.makedirs(os.path.join(output,"slurm/calling/"))
+    if not os.path.exists(os.path.join(output,"slurm/combine/")):
+        os.makedirs(os.path.join(output,"slurm/combine/"))
+    if not os.path.exists(os.path.join(output,"slurm/annotation/")):
+        os.makedirs(os.path.join(output,"slurm/annotation/"))    
     #the prefix of the output is set to the prefix of the bam file
     prefix=args.bam.split("/")[-1]
     prefix=prefix.replace(".bam","")
         
     #run the callers
     input_vcf=""
+    sbatch_ID=[]
     for caller in config["FindSV"]["calling"]:
         general_config=config["FindSV"]["general"]
         caller_config=config["FindSV"]["calling"][caller]
+        #run the FT script
         if caller == "FT":
-            FT=scripts["FindSV"]["calling"][caller].format(account=general_config["account"],output=output,prefix=prefix,time="30:00:00",FT_path=caller_config["FT_path"],bam_path=args.bam,minimum_suporting=caller_config["minimum_supporting_pairs"])
-            print(FT)
-            input_vcf += "{}/{}_FT_inter_chromosomal.vcf ".format(output,prefix)
-            input_vcf += "{}/{}_FT_intra_chromosomal.vcf ".format(output,prefix)
+            job_name="FT_{}".format(prefix)
+            process_files=os.path.join(output,"slurm/calling/",job_name)
+            #generate the header
+            FT=scripts["FindSV"]["header"].format(account=general_config["account"],time="30:00:00",name=job_name,filename=process_files)
+            output_prefix=os.path.join(output,"{}_FT".format(prefix))
+            #generate the body
+            FT += scripts["FindSV"]["calling"][caller].format(output=output_prefix,FT_path=caller_config["FT_path"],bam_path=args.bam,minimum_suporting=caller_config["minimum_supporting_pairs"])
+            sbatch_ID.append(submitSlurmJob( os.path.join(output,"slurm/calling/FT_{}.slurm".format(prefix)),FT) )
+            input_vcf += "{}_inter_chromosomal.vcf ".format(output_prefix)
+            input_vcf += "{}_intra_chromosomal.vcf ".format(output_prefix)
+        #run cnvnator
         elif caller =="CNVnator":
-            CNVNator=scripts["FindSV"]["calling"][caller].format(account=general_config["account"],output=output,prefix=prefix,time="30:00:00",CNVnator_path=caller_config["CNVnator_path"],bam_path=args.bam,bin_size=caller_config["bin_size"],reference_dir=caller_config["reference_dir"])
-            print(CNVNator)
-            input_vcf += "{}/{}_CNVnator.vcf ".format(output,prefix)
+            job_name="CNVnator_{}".format(prefix)
+            process_files=os.path.join(output,"slurm/calling/",job_name)
+            CNVNator=scripts["FindSV"]["header"].format(account=general_config["account"],time="30:00:00",name=job_name,filename=process_files)
+            output_prefix=os.path.join(output,prefix)
+            CNVNator += scripts["FindSV"]["calling"][caller].format(output=output_prefix,CNVnator_path=caller_config["CNVnator_path"],bam_path=args.bam,bin_size=caller_config["bin_size"],reference_dir=caller_config["reference_dir"],CNVnator2vcf_path=caller_config["CNVnator2vcf_path"])
+            input_vcf += "{}_CNVnator.vcf ".format(output_prefix)
+            sbatch_ID.append(submitSlurmJob( os.path.join(output,"slurm/calling/CNVnator_{}.slurm".format(prefix)) ,CNVNator) )
+            
     #combine module; combine all the caller modules into one VCF
-    combine_config=config["FindSV"]["combine"]
-    combine=scripts["FindSV"]["combine"]["combine"].format(account=general_config["account"],output=output,prefix=prefix,time="3:00:00",merge_vcf_path=combine_config["merge_vcf_path"],input_vcf=input_vcf,contig_sort_path=combine_config["contig_sort_path"],bam_path=args.bam)
-    print(combine)
+    job_name="combine_{}".format(prefix)
+    process_files=os.path.join(output,"slurm/combine/",job_name)
+    merge_VCF_path=os.path.join(programDirectory,"internal_scripts","mergeVCF.py")
+    contig_sort=os.path.join(programDirectory,"internal_scripts","contigSort.py")
+    combine=scripts["FindSV"]["header"].format(account=general_config["account"],time="3:00:00",name=job_name,filename=process_files)
+    combine += scripts["FindSV"]["combine"]["combine"].format(output=output_prefix,merge_vcf_path=merge_VCF_path,input_vcf=input_vcf,contig_sort_path=contig_sort,bam_path=args.bam,slurm_IDs=":".join(sbatch_ID))
+    combine_ID=submitSlurmJob( os.path.join(output,"slurm/combine/combine_{}.slurm".format(prefix)) , combine)
     
     #annotation module; filter and annotate the samples
     annotation_config=config["FindSV"]["annotation"]
+    job_name="annotation_{}".format(prefix)
+    process_files=os.path.join(output,"slurm/annotation/",job_name)
+    annotation = scripts["FindSV"]["header"].format(account=general_config["account"],time="10:00:00",name=job_name,filename=process_files)
     
-    annotation=scripts["FindSV"]["annotation"]["header"].format(account=general_config["account"],output=output,prefix=prefix,time="10:00:00")
-    annotation += scripts["FindSV"]["annotation"]["DB"].format(query_script=annotation_config["DB"]["DB_script_path"],output=output,prefix=prefix,db_folder_path=annotation_config["DB"]["DB_path"])
+    output_prefix=os.path.join(output,prefix)
+    annotation += scripts["FindSV"]["annotation"]["header"].format(combine_script_id=combine_ID)
+    annotation += scripts["FindSV"]["annotation"]["DB"].format(query_script=annotation_config["DB"]["DB_script_path"],output=output_prefix,db_folder_path=annotation_config["DB"]["DB_path"])
     cache_dir=""
     if not annotation_config["VEP"]["cache_dir"] == "":
         cache_dir=" --dir {}".format(annotation_config["VEP"]["cache_dir"])
-    annotation += scripts["FindSV"]["annotation"]["VEP"].format(vep_path=annotation_config["VEP"]["VEP.pl_path"],output=output,prefix=prefix,port=annotation_config["VEP"]["port"],cache_dir=cache_dir)
-    annotation += scripts["FindSV"]["annotation"]["GENMOD"].format(genmod_score_path=annotation_config["GENMOD"]["GENMOD_rank_model_path"],output=output,prefix=prefix)
-    annotation += scripts["FindSV"]["annotation"]["cleaning"].format(output=output,prefix=prefix,VCFTOOLS_path=annotation_config["VCFTOOLS"]["VCFTOOLS_path"])
-    print(annotation)
-    return(None)
+    annotation += scripts["FindSV"]["annotation"]["VEP"].format(vep_path=annotation_config["VEP"]["VEP.pl_path"],output=output_prefix,port=annotation_config["VEP"]["port"],cache_dir=cache_dir)
+    annotation += scripts["FindSV"]["annotation"]["GENMOD"].format(genmod_score_path=annotation_config["GENMOD"]["GENMOD_rank_model_path"],output=output_prefix)
+    clean_VCF_path=os.path.join(programDirectory,"internal_scripts","cleanVCF.py")
+    annotation += scripts["FindSV"]["annotation"]["cleaning"].format(output=output_prefix,VCFTOOLS_path=clean_VCF_path)
+    submitSlurmJob( os.path.join(output,"slurm/annotation/annotation_{}.slurm".format(prefix)) , annotation)
 
 
 
--- FindSV_modules.py
+++ FindSV_modules.py
@@ -2,70 +2,56 @@ import yaml
 
 
 def main():
-#the FT script
-    FT="""#! /bin/bash -l
+#Slurm header
+    header="""#! /bin/bash -l
 #SBATCH -A {account}
-#SBATCH -o {output}/slurm/calling/FT_{prefix}.out
-#SBATCH -e {output}/slurm/calling/FT_{prefix}.err
-#SBATCH -J FT_{prefix}.job
+#SBATCH -o {filename}.out
+#SBATCH -e {filename}.err
+#SBATCH -J {name}.job
 #SBATCH -p core
-#SBATCH -t {time}
+#SBATCH -t {time}"""
 
-{FT_path} --sv  --bam {bam_path} --auto --minimum-supporting-pairs {minimum_suporting} --output {output}/{prefix}_FT
-rm {output}/{prefix}_FT.tab"""
+    #the FT script
+    FT="""
+{FT_path} --sv  --bam {bam_path} --auto --minimum-supporting-pairs {minimum_suporting} --output {output}
+rm {output}.tab"""
     
 #the cnvnator script
-    CNVnator="""#! /bin/bash -l
-#SBATCH -A {account}
-#SBATCH -o {output}/slurm/calling/CNV_{prefix}.out
-#SBATCH -e {output}/slurm/calling/CNV_{prefix}.err
-#SBATCH -J CNV_{prefix}.job
-#SBATCH -p core
-#SBATCH -t {time}
-
-{CNVnator_path} -root {output}/{prefix}.root -tree {bam_path}
-{CNVnator_path} -root {output}/{prefix}.root -his {bin_size} -d {reference_dir}
-{CNVnator_path} -root {output}/{prefix}.root -stat {bin_size} >> {output}/{prefix}.cnvnator.log
-{CNVnator_path} -root {output}/{prefix}.root -partition {bin_size}
-{CNVnator_path} -root {output}/{prefix}.root -call {bin_size} > {output}/{prefix}.cnvnator.out
-{CNVnator_path} {output}/{prefix}.cnvnator.out  >  {output}/{prefix}_CNVnator.vcf
-rm {output}/{prefix}.root
-rm {output}/{prefix}.cnvnator.out
+    CNVnator="""
+{CNVnator_path} -root {output}.root -tree {bam_path}
+{CNVnator_path} -root {output}.root -his {bin_size} -d {reference_dir}
+{CNVnator_path} -root {output}.root -stat {bin_size} >> {output}.cnvnator.log
+{CNVnator_path} -root {output}.root -partition {bin_size}
+{CNVnator_path} -root {output}.root -call {bin_size} > {output}.cnvnator.out
+{CNVnator2vcf_path} {output}.cnvnator.out  >  {output}_CNVnator.vcf
+rm {output}.root
+rm {output}.cnvnator.out
 """
     calling={"FT":FT,"CNVnator":CNVnator}
     
 #The combine script
-    combine="""#! /bin/bash
-#SBATCH -A {account}
-#SBATCH -o {output}/slurm/combine/combine_{prefix}.out
-#SBATCH -e {output}/slurm/combine/combine_{prefix}.err
-#SBATCH -J combine_{prefix}.job
-#SBATCH -p core
-#SBATCH -t {time}
+    combine="""
+#SBATCH -d afterok:{slurm_IDs}
 
-python {merge_vcf_path} --vcf {input_vcf} > {output}/{prefix}_FindSV.unsorted.vcf
-python {contig_sort_path} --vcf {output}/{prefix}_FindSV.unsorted.vcf --bam {bam_path} > {output}/{prefix}_FindSV.vcf
-rm {output}/{prefix}_FindSV.unsorted.vcf"""
+python {merge_vcf_path} --vcf {input_vcf} > {output}_FindSV.unsorted.vcf
+python {contig_sort_path} --vcf {output}_FindSV.unsorted.vcf --bam {bam_path} > {output}_FindSV.vcf
+rm {output}_FindSV.unsorted.vcf"""
     combine={"combine":combine}
     
 #The annotation header
-    header="""#SBATCH -A {account}
-#SBATCH -o {output}/slurm/annotation/annotation_{prefix}.out
-#SBATCH -e {output}/slurm/annotation/annotation_{prefix}.err
-#SBATCH -J annotation_{prefix}.job
-#SBATCH -p core
-#SBATCH -t {time}
+    annotation_header="""
+#SBATCH -d afterok:{combine_script_id}
 
 """
 #the DB section
-    DB="python {query_script} --variations {output}/{prefix}_FindSV.vcf --db {db_folder_path} > {output}/{prefix}_frq.vcf\n"
+    DB="python {query_script} --variations {output}_FindSV.vcf --db {db_folder_path} > {output}_frq.vcf\n"
 #the vep section
-    VEP="perl {vep_path} --cache --force_overwrite --poly b -i {output}/{prefix}_frq.vcf -o {output}/{prefix}_vep.vcf --buffer_size 5 --port {port} --vcf --per_gene --format vcf  {cache_dir} -q\n"
+    VEP="perl {vep_path} --cache --force_overwrite --poly b -i {output}_frq.vcf -o {output}_vep.vcf --buffer_size 5 --port {port} --vcf --per_gene --format vcf  {cache_dir} -q\n"
 #the genmod section
-    GENMOD="genmod score -c {genmod_score_path} {output}/{prefix}_vep.vcf > {output}/{prefix}_vep.vcf.tmp\nmv {output}/{prefix}_vep.vcf.tmp {output}/{prefix}_vep.vcf\n"
-#the cleaning sections
-    cleaning="{VCFTOOLS_path} --recode --recode-INFO-all --remove-filtered-all --vcf {output}/{prefix}_vep.vcf --stdout > {output}/{prefix}_FindSV_clean.vcf \n"
-    filter={"header":header,"VEP":VEP,"DB":DB,"GENMOD":GENMOD,"cleaning":cleaning}
+    GENMOD="genmod score -c {genmod_score_path} {output}_vep.vcf > {output}_vep.vcf.tmp\nmv {output}_vep.vcf.tmp {output}_vep.vcf\n"
     
-    scripts={"FindSV":{"calling":calling,"annotation":filter,"combine":combine}}
+    cleaning="python {VCFTOOLS_path} --vcf {output}_vep.vcf > {output}_FindSV_clean.vcf \n"
+    filter={"header":annotation_header,"VEP":VEP,"DB":DB,"GENMOD":GENMOD,"cleaning":cleaning}
+    
+    scripts={"FindSV":{"calling":calling,"annotation":filter,"combine":combine,"header":header}}
     return(scripts)
