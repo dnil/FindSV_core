911456bb97d09cd13ef6cb4eef023f004d001274
Jesper Eisfeldt <J35P312@users.noreply.github.com>
2016-03-08 00:50:32 +0100

    Update FindSV.py

--- FindSV.py
+++ FindSV.py
@@ -98,6 +98,9 @@ def main(args):
     merge_VCF_path=os.path.join(programDirectory,"internal_scripts","mergeVCF.py")
     contig_sort=os.path.join(programDirectory,"internal_scripts","contigSort.py")
     combine=scripts["FindSV"]["header"].format(account=general_config["account"],time="3:00:00",name=job_name,filename=process_files)
+    #if we are on Uppmax, the samtools module is loaded, otherise it is assumed to be correctly installed
+    if not general_config["UPPMAX"] == "":
+        combine +=scripts["FindSV"]["UPPMAX"].format(modules="bioinfo-tools samtools")
     combine += scripts["FindSV"]["combine"]["combine"].format(output=output_prefix,merge_vcf_path=merge_VCF_path,input_vcf=input_vcf,contig_sort_path=contig_sort,bam_path=args.bam,slurm_IDs=":".join(sbatch_ID))
     combine_ID=submitSlurmJob( os.path.join(output,"slurm/combine/combine_{}.slurm".format(prefix)) , combine)
     
