98ec050674baacdb843d9c4a33f80418f5fc8720
Daniel Nilsson <daniel.nilsson@scilifelab.se>
2016-04-04 13:26:21 +0200

    added an alternative root binary distro.

--- internal_scripts/install_cnvnator.sh
+++ internal_scripts/install_cnvnator.sh
@@ -1,7 +1,22 @@
 cd $1
 #install root and ad it to path
-wget https://root.cern.ch/download/root_v5.34.34.Linux-ubuntu14-x86_64-gcc4.8.tar.gz
-tar -xvf root_v5.34.34.Linux-ubuntu14-x86_64-gcc4.8.tar.gz
+
+if [ `uname -o` == "GNU/Linux" ] ; 
+then if [ `uname -p` == "x86_64" ] ; 
+    then 
+	gcc --version |grep 4.4 > /dev/null; 
+	if [ $? -eq 0 ] ; 
+	then
+	    rootver=root_v5.34.34.Linux-slc6-x86_64-gcc4.4	   
+	else
+	    rootver=root_v5.34.34.Linux-ubuntu14-x86_64-gcc4.8
+	fi 
+    fi
+fi
+
+wget https://root.cern.ch/download/${rootver}.tar.gz
+tar -xvf ${rootver}.tar.gz
+
 #cd root/bin/
 #source thisroot.sh
 #cd $1
